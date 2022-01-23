c66424be16e29ad2a80443b98bfcad0d6ad753de
Gokulpradap <94739280+Gokulpradap@users.noreply.github.com>
2022-01-23 22:52:37 +0530

    Add files via upload

---
+++ day 21/script.js
@@ -0,0 +1,54 @@
+
+let time = 11;
+setTimeout(() => {
+    document.querySelector(".data").innerHTML = "Hello";
+    setTimeout(() => {
+        time--;
+        document.querySelector(".data").innerHTML = time;
+        setTimeout(() => {
+            time--;
+            document.querySelector(".data").innerHTML = time;
+            setTimeout(() => {
+                time--;
+                document.querySelector(".data").innerHTML = time;
+                setTimeout(() => {
+                    time--;
+                    document.querySelector(".data").innerHTML = time;
+                    setTimeout(() => {
+                        time--;
+                        document.querySelector(".data").innerHTML = time;
+                        setTimeout(() => {
+                            time--;
+                            document.querySelector(".data").innerHTML = time;
+                            setTimeout(() => {
+                                time--;
+                                document.querySelector(".data").innerHTML = time;
+                                setTimeout(() => {
+                                    time--;
+                                    document.querySelector(".data").innerHTML = time;
+                                    setTimeout(() => {
+                                        time--;
+                                        document.querySelector(".data").innerHTML = time;
+                                        setTimeout(() => {
+                                            time--;
+                                            document.querySelector(".data").innerHTML = time;
+                                        }, 1000);
+                                        setTimeout(() => {
+                                            document.querySelector(".data").innerHTML = "All Is Well";
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);                            
+                              }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+    
+                            
+                            
+                            
+                            
+                            
